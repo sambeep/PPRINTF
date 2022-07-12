b317b1a90367cfe4f88bf70d342060f08880c19e
sambeep <samsonkinyanjui11@gmail.com>
2022-07-12 06:28:09 -0700

    Task 0.

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
-#include "mainn.h"
-
+#include "main.h"
 /**
+ *
  * _printf - is a function that selects the correct function to print.
  * @format: identifier to look for
  * Return: the length of the string.
